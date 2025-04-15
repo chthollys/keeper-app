9d56c467abc76f38fba948c11790bd91474b3772
chthollys <jason130105@gmail.com>
2025-04-15 23:19:14 +0700

    Added normal functionality
    Create, Read, and Delete Functionality worked as expected.
    Pending Update Func.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  const appendNote = (noteTitle, noteContent) => {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        {
+          title: noteTitle,
+          content: noteContent,
+        }
+      ]
+    });
+  }
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((_, index) => index !== id);
+    })
+  };
+
+  // useEffect(() => console.log(notes), [notes]);
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onSubmitApp={appendNote} />
+      {notes.map((item, index) => (
+        <Note key={index} id={index} title={item.title} content={item.content} onClickDelete={deleteNote}/>
+      ))}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea({ onSubmitApp }) {
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const trackValue = (event) => {
+    const {name, value} = event.target;
+    // console.log(name," => ", value);
+    if (name == "title") {
+      setTitle(value);
+    } else if (name == "content") {
+      setContent(value);
+    } else {
+      console.error("No target with the given name of : ", name)
+    }
+  };
+
+  const isEmpty = (str) => str.trim() === "";
+
+  const onSubmitAction = (event) => {
+    event.preventDefault();
+    const isFormIncomplete = isEmpty(title) || isEmpty(content);
+    if (isFormIncomplete == true) {
+      setErrorMessage("Please complete the note");
+      return;
+    }
+    try {
+      setErrorMessage(""); // Clear error message on successful submission
+      onSubmitApp(title, content);
+      setTitle("");
+      setContent("");
+      // console.log("Succesfully requested onSubmitAction");
+    } catch (error) {
+      console.error("Error occurred while submitting the note:", error);
+    }
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form onSubmit={onSubmitAction}>
+        <input
+          name="title"
+          placeholder="Title"
+          value={title}
+          onChange={trackValue}
+        />
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={content}
+          onChange={trackValue}
+        />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
