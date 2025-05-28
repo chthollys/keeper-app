0f20256d7234e34a8fb88c81b73f706f22131a6a
chthollys <jason130105@gmail.com>
2025-05-28 15:19:07 +0700

    added hooks function
    Added useRef implementation instead of double binding way
    
    Signed-off-by: chthollys <jason130105@gmail.com>

--- front-end/src/components/CreateArea.tsx
+++ front-end/src/components/CreateArea.tsx
@@ -1,40 +1,31 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState, useRef } from "react";
 
 interface CreateAreaProps {
   onCreateNote: (title: string, content: string) => void;
 }
 
-function CreateArea({ onCreateNote } : CreateAreaProps) {
-
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+function CreateArea({ onCreateNote }: CreateAreaProps) {
+  const title = useRef<HTMLInputElement>(null);
+  const content = useRef<HTMLTextAreaElement>(null);
   const [errorMessage, setErrorMessage] = useState("");
-
-  const trackValue = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {name, value} = event.target;
-    // console.log(name," => ", value);
-    if (name == "title") {
-      setTitle(value);
-    } else if (name == "content") {
-      setContent(value);
-    } else {
-      console.error("No target with the given name of : ", name)
-    }
-  };
 
   const isEmpty = (str: string) => str.trim() === "";
 
   const onSubmitAction = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const isFormIncomplete = isEmpty(title) || isEmpty(content);
-    if (isFormIncomplete == true) {
+    let inputTitle = title.current?.value ?? "";
+    let inputContent = content.current?.value ?? "";
+    const isFormIncomplete =
+      isEmpty(inputTitle) ||
+      isEmpty(inputContent);
+    if (isFormIncomplete) {
       setErrorMessage("Please complete the note");
       return;
     }
     setErrorMessage(""); // Clear error message on successful submission
-    onCreateNote(title, content);
-    setTitle("");
-    setContent("");
+    onCreateNote(inputTitle, inputContent);
+    if (title.current) title.current.value = "";
+    if (content.current) content.current.value = "";
   };
 
   return (
@@ -42,18 +33,16 @@ function CreateArea({ onCreateNote } : CreateAreaProps) {
       <form className="main-form" onSubmit={onSubmitAction}>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
         <input
+          ref={title}
           name="title"
           placeholder="Title"
-          value={title}
-          onChange={trackValue}
           aria-label="Note Title"
         />
         <textarea
+          ref={content}
           name="content"
           placeholder="Take a note..."
           rows={3}
-          value={content}
-          onChange={trackValue}
           aria-label="Note Content"
         />
         <button type="submit">Add</button>
--- front-end/src/components/Note.tsx
+++ front-end/src/components/Note.tsx
@@ -1,51 +1,49 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useState, useRef } from "react";
 
 interface NoteProps {
   id: string;
   title: string;
   content: string;
   onClickDelete: (id: string) => void;
-  onClickEdit: (id: string, updatedData: { title: string, content: string }) => void;
+  onClickEdit: (
+    id: string,
+    updatedData: { title: string; content: string }
+  ) => void;
 }
 
-function Note({ id, title, content, onClickDelete, onClickEdit}: NoteProps) {
+function Note({ id, title, content, onClickDelete, onClickEdit }: NoteProps) {
+  const editedTitle = useRef<HTMLInputElement>(null);
+  const editedContent = useRef<HTMLTextAreaElement>(null);
+
   const [isEditing, setIsEditing] = useState(false);
-  const [editedTitle, setEditedTitle] = useState(title);
-  const [editedContent, setEditedContent] = useState(content);
   const [errorMessage, setErrorMessage] = useState("");
 
   const isEmpty = (str: string) => str.trim() === "";
 
   const editTrueState = () => {
-    setEditedTitle(title);
-    setEditedContent(content);
     setIsEditing(true);
-  };
-
-  const trackEditedValue = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {name, value} = event.target;
-    if (name == "editedTitle") {
-      setEditedTitle(value);
-    } else if (name == "editedContent") {
-      setEditedContent(value);
-    } else {
-      console.error("No target with the given name of : ", name)
-    }
+    setErrorMessage("");
   };
 
   const onSubmitSecondary = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const isFormIncomplete = isEmpty(editedTitle) || isEmpty(editedContent);
+    let inputTitle = editedTitle.current?.value ?? "";
+    let inputContent = editedContent.current?.value ?? "";
+    const isFormIncomplete =
+      isEmpty(inputTitle) ||
+      isEmpty(inputContent);
     if (isFormIncomplete) {
       setErrorMessage("Please complete the note.");
       return;
     }
-    setErrorMessage("");
     try {
-      onClickEdit(id, { title: editedTitle, content: editedContent });
+      onClickEdit(id, { title: inputTitle, content: inputContent });
     } catch (error) {
       if (error instanceof Error) {
-        console.error("Error editing note: ", (error as any)?.response?.data || error.message);
+        console.error(
+          "Error editing note: ",
+          (error as any)?.response?.data || error.message
+        );
       } else {
         console.error("An unknown error occurred.");
       }
@@ -55,36 +53,26 @@ function Note({ id, title, content, onClickDelete, onClickEdit}: NoteProps) {
 
   return (
     <div className="note">
-      { isEditing ? (
+      {isEditing ? (
         <form className="secondary-form" onSubmit={onSubmitSecondary}>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
           <input
+            ref={editedTitle}
             name="editedTitle"
-            placeholder="Title"
-            value={editedTitle}
-            onChange={trackEditedValue}
+            placeholder={title}
             aria-label="Edit Note Title"
             className="h1"
           />
           <textarea
+            ref={editedContent}
             name="editedContent"
-            placeholder="Take a note..."
-            value={editedContent}
-            onChange={trackEditedValue}
+            placeholder={content}
             aria-label="Edit Note Content"
             className="paragraph"
           />
           <div className="button-container">
             <button type="submit">SAVE</button>
-            <button
-              onClick={() => {
-                setEditedTitle(title);
-                setEditedContent(content);
-                setIsEditing(false);
-              }}
-            >
-              CANCEL
-            </button>
+            <button onClick={editTrueState}>CANCEL</button>
           </div>
         </form>
       ) : (
@@ -92,13 +80,13 @@ function Note({ id, title, content, onClickDelete, onClickEdit}: NoteProps) {
           <h1>{title}</h1>
           <p>{content}</p>
           <div className="button-container">
-              <button onClick={editTrueState}>EDIT</button>
-              <button onClick={() => onClickDelete(id)}>DELETE</button>
+            <button onClick={editTrueState}>EDIT</button>
+            <button onClick={() => onClickDelete(id)}>DELETE</button>
           </div>
         </>
       )}
     </div>
   );
-};
+}
 
 export default Note;
